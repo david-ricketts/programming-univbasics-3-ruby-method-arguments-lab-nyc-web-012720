18cb6acd0a23288b87161a73afce2e6fd2972699
David Ricketts <david.ricketts119@gmail.com>
2020-01-06 00:26:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,56 +4,14 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 
+def introduction(name)
+  puts "Hi, my name is #{name}"
+end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def introduction_with_language(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learing to program in #{language}""
+end
 
 
 
